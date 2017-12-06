3a2f3eb4c55cccd14418c84fb4b5d9f1e91199f2
afanasyevalin <afanasyevalin@gmail.com>
2017-12-06 20:36:44 +0500

    qqq

---
+++ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 1;
+
+            SwapInt(ref a, ref b);
+            Console.WriteLine("a={0}, b={1}", a, b);
+        }
+        /// <summary>
+        /// Обмен значений переменных
+        /// </summary>
+        /// <param name="a">Первая переменная</param>
+        /// <param name="b">Вторая переменная</param>
+        private static void SwapInt(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
